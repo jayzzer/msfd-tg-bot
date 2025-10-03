3a5d0c63d9a7fa73e68414f55f3146873063d40c
Pavel Pushkarev <pavel10_pushkarev@mail.ru>
2025-10-03 18:03:47 +0300

    add masks

--- src/masks.ts
+++ src/masks.ts
@@ -12,19 +12,19 @@ export const masks: Record<UserType, MaskOption> = {
     scale: 1,
   },
   expert: {
-    path: path.join(__dirname, "../assets/masks/designer.png"),
+    path: path.join(__dirname, "../assets/masks/expert.png"),
     scale: 1,
   },
   model: {
-    path: path.join(__dirname, "../assets/masks/designer.png"),
+    path: path.join(__dirname, "../assets/masks/model.png"),
     scale: 1,
   },
   artist: {
-    path: path.join(__dirname, "../assets/masks/designer.png"),
+    path: path.join(__dirname, "../assets/masks/artist.png"),
     scale: 1,
   },
   participant: {
-    path: path.join(__dirname, "../assets/masks/designer.png"),
+    path: path.join(__dirname, "../assets/masks/participant.png"),
     scale: 1,
   },
 };
